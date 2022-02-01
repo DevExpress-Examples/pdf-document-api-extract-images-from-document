b2141c072ea158f2557fd7178351d867898b067a
DevExpressExampleBot <fakeEmail>
2022-02-01 21:34:45 +0300

    Source auto update [skip ci]

--- VB/PdfProcessorGetImages/Program.vb
+++ VB/PdfProcessorGetImages/Program.vb
@@ -1,18 +1,19 @@
-﻿#Region "#Reference"
+'#Region "#Reference"
 Imports System
 Imports System.Collections.Generic
 Imports System.Drawing
 Imports DevExpress.Pdf
+
 ' ...
-#End Region ' #Reference
-
+'#End Region  ' #Reference
 Namespace PdfProcessorGetImages
-    Friend Class Program
-        #Region "#Code"
-        Shared Sub Main(ByVal args() As String)
-            Dim processor As New PdfDocumentProcessor()
-            processor.LoadDocument("..\\..\\Demo.pdf")
 
+    Friend Class Program
+
+'#Region "#Code"
+        Shared Sub Main(ByVal args As String())
+            Dim processor As PdfDocumentProcessor = New PdfDocumentProcessor()
+            processor.LoadDocument("..\\..\\Demo.pdf")
             Dim xCount As Integer = 8 ' The page rotation angle is 90 degrees.
             Dim yCount As Integer = 2
             Dim cardWidth As Double = 150.5 ' Measured in points (equals 2.09 inches).
@@ -20,26 +21,27 @@ Namespace PdfProcessorGetImages
             Dim xMargin As Double = 122 ' Measured in points (equals 1.694 inches).
             Dim yMargin As Double = 77 ' Measured in points (equals 1.069 inches).
             Dim yCoord As Double = yMargin
-
             Dim y As Integer = 0
-            Do While y < yCount
+            While y < yCount
                 Dim xCoord As Double = xMargin
                 Dim x As Integer = 0
-                Do While x < xCount
-                    Dim area As New PdfDocumentArea(1, New PdfRectangle(xCoord, yCoord, xCoord + cardWidth, yCoord + cardHeight))
+                While x < xCount
+                    Dim area As PdfDocumentArea = New PdfDocumentArea(1, New PdfRectangle(xCoord, yCoord, xCoord + cardWidth, yCoord + cardHeight))
                     Dim bitmaps As IList(Of Bitmap) = processor.GetImages(area)
                     If bitmaps.Count <> 0 Then
                         bitmaps(0).Save(String.Format("{0}_{1}.bmp", x, y))
                         bitmaps(0).Dispose()
                     End If
+
                     Console.WriteLine(bitmaps.Count.ToString())
                     x += 1
                     xCoord += cardWidth
-                Loop
+                End While
+
                 y += 1
                 yCoord += cardHeight
-            Loop
+            End While
         End Sub
-        #End Region ' #Code
+'#End Region  ' #Code
     End Class
 End Namespace
